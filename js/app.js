aed78ed789a97c2a115b97ccf88ada1716051f3e
Sarah Scott <s.j.scott84@gmail.com>
2016-01-23 14:07:42 -0800

    Add observable array and list in DOM

--- js/app.js
+++ js/app.js
@@ -1,15 +1,37 @@
 'use strict'
-
 var map;
 
 var spots = [
-	{name: "Place 1",
-	latlng: {lat: 37.7847191, lng: -122.414172}
+	{
+		name: "Place 2",
+		latlng: {lat: 37.7847191, lng: -122.414172}
 	},
-	{name: "Place 2",
-	latlng: {lat: 37.7812488, lng: -122.411304}
+	{
+		name: "Place 4",
+		latlng: {lat: 37.7812488, lng: -122.411304}
+	},
+	{
+		name: "Place 6",
+		latlng: {lat: 37.7812490, lng: -122.411310}
 	}
 ];
+
+var Place = function(data){
+	this.name = ko.observable(data.name);
+	this.latlng = ko.observable(data.latlng);
+}
+
+var ViewModel = function(){
+	var self = this;
+
+	this.listView = ko.observableArray([]);
+
+	spots.forEach(function(viewItem){
+		self.listView.push(new Place(viewItem));
+	})
+}
+
+ko.applyBindings(new ViewModel());
 
 function initMap(){
 	map = new google.maps.Map(document.getElementById('map'),{
