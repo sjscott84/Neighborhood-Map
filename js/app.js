cbb9fa720b9df602be363e623b3e1538f1df952f
Sarah Scott <s.j.scott84@gmail.com>
2016-01-26 15:14:19 -0800

    Search places by type

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 var map;
+var startPoint = {lat:37.773972, lng: -122.431297};
 var searchBox;
 var places;
 var listView;
@@ -8,7 +9,7 @@ var markers = [];
 
 function initMap(){
 	map = new google.maps.Map(document.getElementById('map'),{
-		center: {lat:37.773972, lng: -122.431297},
+		center: startPoint,
 		zoom: 12,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	});
@@ -60,6 +61,35 @@ function addSearch (){
 		map.fitBounds(bounds);
 	});
 });
+	findThings();
+}
+
+function findThings (){
+
+	var service = new google.maps.places.PlacesService(map);
+	service.nearbySearch({
+		location: startPoint,
+		radius: '5000',
+		type: ['museum']
+		}, callback);
+
+	function callback(results, status){
+		if (status === google.maps.places.PlacesServiceStatus.OK) {
+			for (var i = 0; i < results.length; i++) {
+					var place = results[i];
+					var name = place.name;
+					var position = place.geometry.location;
+					var icon = {
+						url: 'http://maps.gstatic.com/mapfiles/markers2/markerA.png',
+						origin: new google.maps.Point(0, 0),
+						anchor: new google.maps.Point(17, 34),
+					};
+
+					view.addPlace(name, position, icon);
+					//createMarker(results[i]);)
+			}
+		}
+	}
 }
 
 var outdoors = ['park', 'zoo'];
@@ -90,14 +120,20 @@ var ViewModel = function(){
 	};
 
 	self.seePlaces = function (){
+		var forSearch = [];
 		$('input[name="whatToDo"]:checked').each(function(){
-			console.log(this.value);
+			var input = this.value;
+			forSearch.push(input);
 		});
+			console.log(forSearch);
 	};
+
+
 
 };
 
 view = new ViewModel();
 
 ko.applyBindings(view);
+
 
