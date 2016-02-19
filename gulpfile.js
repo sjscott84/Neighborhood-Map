c916d44856d96a01bd9a926cdc1ebd6d3f0ea18f
Sarah Scott <s.j.scott84@gmail.com>
2016-02-18 16:59:56 -0800

    Add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,30 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano');
+var gulpIf = require('gulp-if');
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('useref', function(){
+	return gulp.src('app/*.html')
+		.pipe(useref())
+		// Minifies only if it's a JavaScript file
+		.pipe(gulpIf('*.js', uglify()))
+		.pipe(gulpIf('*.css', cssnano()))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('uglify', function(){
+	return gulp.src('app/*.html')
+		.pipe(gulpIf('*.html', htmlmin()))
+		.pipe(gulp.dest('dist'));
+})
 
 gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'app'
-    },
-  })
+	browserSync.init({
+		server: {
+			baseDir: 'app'
+		},
+	})
 })
